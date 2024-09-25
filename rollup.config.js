2f59fe1e3f37f76958211b2497d25138a850f5be
Long Li <lilong@gmail.com>
2024-09-25 10:00:18 +0900

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,61 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
-export default {
-  input: 'index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'umd',
-    name: 'bsv',
-    globals: {
-      buffer: 'Buffer'
-    }
+export default [
+  // CommonJS (for Node) build
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/bsv.cjs.js',
+      format: 'cjs',
+      exports: 'auto'
+    },
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      commonjs(),
+      json()
+    ],
+    external: ['buffer', 'crypto']
   },
-  plugins: [
-    resolve({
-      preferBuiltins: true,
-      browser: true
-    }),
-    commonjs(),
-    json()
-  ],
-  external: ['buffer']
-};
+
+  // ES module (for bundlers) build
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/bsv.esm.js',
+      format: 'es'
+    },
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      commonjs(),
+      json()
+    ],
+    external: ['buffer', 'crypto']
+  },
+
+  // Browser-friendly UMD build
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/bsv.umd.js',
+      format: 'umd',
+      name: 'bsv',
+      globals: {
+        buffer: 'Buffer',
+        crypto: 'crypto'
+      }
+    },
+    plugins: [
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      }),
+      commonjs(),
+      json()
+    ]
+  }
+];
